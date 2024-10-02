e8415bf7a9008b68182f959c14b2d71d8435ce4d
Muhammad <04muhammadimran@gmail.com>
2024-10-02 14:45:35 -0400

    Added 3 activities for part 2

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.androidassignments;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,12 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
-    private Button login_button;
-    private EditText emailEditText;
+    Button loginButton;
+    EditText emailText;
+    EditText passwordText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +37,31 @@ public class LoginActivity extends AppCompatActivity {
         });
         Log.d("LoginActivity", "onCreate called");
 
+        emailText = findViewById(R.id.email_Text);
+        passwordText = findViewById(R.id.password_Text);
+        loginButton = findViewById(R.id.login_button);
+
         sharedPreferences = getSharedPreferences("LoginPrefs",MODE_PRIVATE);
 
-        string savedEmail = sharedPreferences.getString("DefaultEmail","email@domain.com");
+        String savedEmail = sharedPreferences.getString("DefaultEmail","email@domain.com");
 
-        emailEditText.setText(savedEmail);
+        emailText.setText(savedEmail);
 
-        login_button.setOnClickListener(v -> {
-            String enteredEmail = emailEditText.getText().toString();
-            SharedPreferences.editor editor = sharedPreferences.edit();
+        loginButton.setOnClickListener(v -> {
+            String enteredEmail = emailText.getText().toString();
+            String enteredpassword = passwordText.getText().toString();
+
+            if (!validateEmail(enteredEmail)) {
+                emailText.setError("Invalid email address");
+                return; // Stop further execution if email is invalid
+            }
+
+            if (TextUtils.isEmpty(enteredpassword)) {
+                passwordText.setError("Password cannot be empty");
+                return; // Stop further execution if password is empty
+            }
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("DefaultEmail", enteredEmail);
             editor.apply();
 
@@ -91,5 +115,8 @@ public class LoginActivity extends AppCompatActivity {
         Log.d("LoginActivity","onRestoreInstanceState called");
     }
 
+    private boolean validateEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
 
 }
